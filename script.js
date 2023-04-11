2c0e8bc5f36ab926439ab4a82372a463d2b8b50d
Valerii2889 <118856034+Valerii2889@users.noreply.github.com>
2023-04-11 23:32:23 +0300

    add lesson_14

--- script.js
+++ script.js
@@ -1,245 +1,191 @@
-// const container = document.querySelector(".js-container");
+// https://bundlephobia.com/
 
-// container.addEventListener("click", onClick);
-// function onClick(evt) {
-//   // console.log(evt.currentTarget.dataset.color);
-//   if (!evt.target.classList.contains("js-box")) {
-//     return;
-//   }
-//   console.log(evt.target.dataset.color);
-//   console.log(evt.currentTarget);
-// }
+const cats = [
+  "https://i.ytimg.com/vi/iKA6ZXpGcGQ/maxresdefault.jpg",
+  "https://i.natgeofe.com/n/46b07b5e-1264-42e1-ae4b-8a021226e2d0/domestic-cat_thumb_square.jpg",
+  "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg",
+  "https://i.guim.co.uk/img/media/941093798b256e5d1aa6246a08824bc86f62a31f/0_0_5000_3000/master/5000.jpg?width=1300&quality=85&fit=max&s=ea672fb16df8634901283252059baaf8",
+  "https://nypost.com/wp-content/uploads/sites/2/2022/06/reddit-cats-judging-looks-00.jpg?quality=75&strip=all",
+  "https://gdb.voanews.com/09690000-0a00-0242-5408-08da76ebde16_cx0_cy3_cw99_w1200_r1.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCK8yYjQ6igjrAJyCyXUwV6U7RDkLVc4FVnQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU",
+  "https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU",
+  "https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU",
+  "https://assets.globalwildlife.org/m/3b6039a74fd067ea/webimage-Tigrinia.jpg",
+  "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg",
+  "https://www.nhm.ac.uk/content/dam/nhmwww/discover/wild-cats/margay-two-column.jpg.thumb.768.768.jpg",
+  "https://irs.www.warnerbros.com/gallery-v2-jpeg/cats__dogs_photo_12-14571309.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIyGk9Ds3bdd4B404EcoMDrVUKnU5u9KABOg&usqp=CAU",
+];
 
-// const container = document.querySelector(".js-content");
-// const nameWinner = document.querySelector(".js-winner");
-// let player = "X";
-// let historyX = [];
-// let historyO = [];
-// const wins = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [3, 6, 9],
-//   [1, 5, 9],
-//   [3, 5, 7],
-// ];
+// console.log(_.throttle);
 
-// function createMarkup() {
-//   let markup = "";
-//   for (let i = 1; i < 10; i += 1) {
-//     markup += `<div class="item js-item" data-id="${i}"></div>`;
-//   }
-//   container.innerHTML = markup;
-// }
-// createMarkup();
-
-// container.addEventListener("click", onClick);
-
-// function onClick(evt) {
-//   const { target } = evt;
-//   if (!target.classList.contains("js-item") || target.textContent) {
-//     return;
-//   }
-
-//   const isEndGame = historyO.length + historyX.length === 9;
-//   const id = Number(target.dataset.id);
-//   let result = false;
-
-//   if (player === "X") {
-//     historyX.push(id);
-//     result = isWinner(historyX);
-//   } else {
-//     historyO.push(id);
-//     result = isWinner(historyO);
-//   }
-
-//   target.textContent = player;
-
-//   if (result) {
-//     nameWinner.textContent = `Winner ${player} 😎`;
-//     resetGame();
-//     return;
-//   } else if (isEndGame) {
-//     console.log(`Try again 😱`);
-//     resetGame();
-//     return;
-//   }
-//   player = player === "X" ? "O" : "X";
-// }
-
-// function isWinner(arr) {
-//   return wins.some((item) => item.every((id) => arr.includes(id)));
-// }
-
-// function resetGame() {
-//   createMarkup();
-//   historyX = [];
-//   historyO = [];
-//   player = "X";
-// }
-
-// Перерва до 21.20
-
-// const instance = basicLightbox.create(`
-//     <h1>Dynamic Content</h1>
-//     <p>You can set the content of the lightbox with JS.</p>
-// `);
-// console.log(instance);
-// instance.show()
-
-// const cars = [
-//   {
-//     id: 1,
-//     car: "Honda",
-//     type: "Civic",
-//     price: 12000,
-//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
-//   },
-//   {
-//     id: 2,
-//     car: "Audi",
-//     type: "Q7",
-//     price: 40000,
-//     img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
-//   },
-//   {
-//     car: "BMW",
-//     type: "5 siries",
-//     price: 9000,
-//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
-//   },
-//   {
-//     id: 4,
-//     car: "Honda",
-//     type: "Accord",
-//     price: 20000,
-//     number: "+380000000000",
-//     img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
-//   },
-//   {
-//     id: 5,
-//     car: "Volvo",
-//     type: "XC60",
-//     price: 7000,
-//     img: "https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320",
-//   },
-// ];
-
-// const container = document.querySelector(".js-container");
-
-// const markup = cars.map(
-//   ({ img, car, id }) =>
-//     `<li data-car-id="${id}" class="js-target js-card">
-//     <img class="js-target" src="${img}" alt="${car}" width="200">
-//     <h2 class="js-target">${car}</h2>
-//     </li>`
+// const input = document.querySelector(".js-input");
+// input.addEventListener(
+//   "input",
+//   _.throttle(onSearch, 600, {
+//     leading: true,
+//     trailing: true
+//   })
 // );
 
-// container.insertAdjacentHTML("beforeend", markup.join(""));
-// container.addEventListener("click", onClick);
+// const PASSWORD_LENGTH = 7;
+// input.addEventListener("input", _.debounce(onSearch,700,{
+//     leading: false,
+//     trailing: true,
+// }));
 
-// function onClick(evt) {
-//   const { target } = evt;
-//   if (!target.classList.contains("js-target")) {
-//     return;
+// function onSearch(evt) {
+//     console.log(evt.target.value);
+//   if (evt.target.value.trim().length < PASSWORD_LENGTH) {
+//     evt.target.classList.remove("valid");
+//     evt.target.classList.add("invalid");
+//   } else {
+//     evt.target.classList.remove("invalid");
+//     evt.target.classList.add("valid");
 //   }
-//   // ?? null undefined
-//   // || '' 0 null false undefined NaN
-//   const carId =
-//     target.dataset.carId ?? target.closest(".js-card").dataset.carId;
-//   const currentItem = cars.find(({ id }) => id === Number(carId));
-
-//   if (!currentItem) {
-//     const instance = basicLightbox.create(`
-//     <div class="bg">
-//     <img src="https://www.shutterstock.com/image-vector/no-image-available-icon-ui-600w-1458092489.jpg" width="300" alt="placeholder">
-
-//   </div>
-//   `);
-//     instance.show();
-//     return;
-//   }else{
-//     const instance = basicLightbox.create(`
-//     <div class="bg">
-//     <img src="${currentItem.img}" width="300" alt="${currentItem.car}">
-//     <h2>${currentItem.car}</h2>
-//     <h3>${currentItem.type}</h3>
-//     <p>${currentItem.price}</p>
-//   </div>
-//   `);
-//   instance.show();
-//   }
-
 // }
-// const parent = document.querySelector("#parent");
-// const child = document.querySelector("#child");
-// const descendant = document.querySelector("#descendant");
 
-// parent.addEventListener("click", () => {
-//   alert(
-//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-//   );
-// });
+// const list = document.querySelector('.js-list');
+// const markup = cats.map(cat => `<li><img src="${cat}" loading="lazy" alt="cat" width="600"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum possimus blanditiis fuga harum, aut error vero fugit quas iure et reprehenderit perspiciatis Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum possimus blanditiis fuga harum, aut error vero fugit quas iure et reprehenderit perspiciatis</p></li>`).join('');
 
-// child.addEventListener("click", (event) => {
-//     event.stopPropagation();
-//   alert(
-//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-//   );
-// });
+// list.insertAdjacentHTML('beforeend', markup);
 
-// descendant.addEventListener("click", (event) => {
+// ПЕРЕРВА ДО 21.20
 
-//   alert("Descendant click handler");
-// });
+// Створити функцію яка приймає 1 параметр масив продуктів і повертає мутований масив
+// Потрібно перебрати масив і якщо він має об'єкти в яких дублюються айді то квонтіті цих елементів потрібно сплюсувати
+// а ті обє'кти в яких айді співпав видалити з масиву.
+// (Потрібно мутувати масив, створювати новий не потрібно)
 
-// const form = document.querySelector('form')
-// form
+// const products = [
+//   {
+//     id: "sku1",
+//     qty: 1,
+//   },
+//   {
+//     id: "sku2",
+//     qty: 2,
+//   },
+//   {
+//     id: "sku3",
+//     qty: 3,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 6,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 8,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 6,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 6,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 6,
+//   },
+//   {
+//     id: "sku2",
+//     qty: 19,
+//   },
+//   {
+//     id: "sku4",
+//     qty: 1,
+//   },
+//   {
+//     id: "sku1",
+//     qty: 8,
+//   },
+// ];
 
-const emmetConfig = {
-  "editor.matchBrackets": "never",
-  "[javascript]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-  },
-  "[json]": {
-    "editor.defaultFormatter": "vscode.json-language-features",
-  },
-  "[jsonc]": {
-    "editor.defaultFormatter": "vscode.json-language-features",
-  },
-  "liveServer.settings.donotShowInfoMsg": true,
-  "[html]": {
-    "editor.defaultFormatter": "HookyQR.beautify",
-  },
-  "editor.renderWhitespace": "all",
-  "[scss]": {
-    "editor.defaultFormatter": "HookyQR.beautify",
-  },
-  "[css]": {
-    "editor.defaultFormatter": "HookyQR.beautify",
-  },
-  "editor.detectIndentation": false,
-  "liveServer.settings.donotVerifyTags": true,
-  "redhat.telemetry.enabled": true,
-  "workbench.editor.enablePreview": false,
-  "explorer.confirmDelete": false,
-  "[javascriptreact]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-  },
-  "window.zoomLevel": 4,
-  "[typescript]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-  },
-  "[markdown]": {
-    "editor.wordWrap": "wordWrapColumn",
-  },
-  "emmet.preferences": {},
-};
+// function sortProduct(arr) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     for (let j = i + 1; j < arr.length; j += 1) {
+//       if (arr[i].id === arr[j].id) {
+//         arr[i].qty += arr[j].qty;
+//         arr.splice(j, 1);
+//         j-=1;
+//       }
+//     }
+//   }
+// };
+
+// sortProduct(products);
+// console.log(products);
+// const products = [{
+//     id: 'sku1',
+//     qty: 15,
+// }, {
+//     id: 'sku2',
+//     qty: 21,
+// }, {
+//     id: 'sku3',
+//     qty: 3,
+// }, {
+//     id: 'sku4',
+//     qty: 1,
+// }]
+
+// У кожному масиві, а є унікальний рядок, у якому не повторюються літери. Знайдіть і виведіть цей рядок
+// function findUniq(arr) {
+  // ["abc", "acb", "bac", "foo", "bca", "cab", "cba"]
+//   return arr.find((current, i) => current.split("").every((item) =>
+//           arr.every((comparison, j) => i !== j ? !comparison.includes(item) : true)));
+
+  //   for (let i = 0; i < arr.length; i += 1) {
+  //     const result = arr[i].split("").every((item) => {
+  //       for (let j = 0; j < arr.length; j += 1) {
+  //         if (i !== j) {
+  //           if (!arr[j].includes(item)) {
+  //             return true;
+  //           }else{
+  //             return false
+  //           }
+  //         }
+  //       }
+  //     });
+  //     if(result){
+  //         return arr[i]
+  //     }
+  //   }
+
+//   const arr = ["acb", "bac", "foo", "bca", "cab", "cba"];
+// // from GPT
+// let uniqueString = null;
+
+// for (let str of arr) {
+//   let isUnique = true;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+//       isUnique = false;
+//       break;
+//     }
+//   }
+//   if (isUnique) {
+//     uniqueString = str;
+//     break;
+//   }
+// }
+
+// console.log(uniqueString); // "foo"
+// }
+
+const findUniq =(arr) => arr.find((current, i) => current.split("").every((item) =>
+arr.every((comparison, j) => i !== j ? !comparison.includes(item) : true)));
 
 
-
-// refs.createBtn.addEventListener("click", () => createBoxes(onInputValue()));
-// refs.createBtn.addEventListener("click", createBoxes(onInputValue()));
-//  в чому різниця запису колбеку, один з них працює не правильно
+console.log(findUniq(["abc", "acb", "bac", "foo", "bca", "cab", "cba"])); // === 'foo'
+console.log(findUniq(["fghj", "ghfj", "abcd", "jhgf", "fghj", "fgjh", "ghjf"])); // === 'abcd'
+console.log(findUniq(["qwe", "camp", "acmp", "pmac", "camp", "apmc", "pmac"])); // === 'qwe'
